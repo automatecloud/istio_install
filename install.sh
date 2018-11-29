22509c7661b3f495d4ada80ef31d8a223a95b19c
Andreas Wilke <andreas@twistlock.com>
2018-11-29 10:04:18 +0100

    Update install.sh
    small changes into how the istioctl is called

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 curl -L https://git.io/getLatestIstio | sh -
 cd istio-*
-./bin/istioctl version
-./bin/istioctl kube-inject -f ../istio_install/samples/bookinfo/platform/kube/bookinfo.yaml | kubectl apply -n bookstore -f -
+./istio-*/bin/istioctl version
+./istio-*/bin/istioctl kube-inject -f istio_install/samples/bookinfo/platform/kube/bookinfo.yaml | kubectl apply -n bookstore -f -
