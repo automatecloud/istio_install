32c165e779ef463e98090f62ca94355e3fc1bfff
Andreas Wilke <andreas@twistlock.com>
2018-11-29 20:26:07 +0100

    New script to create the deployment yaml file

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 curl -L https://git.io/getLatestIstio | sh -
 ./istio-*/bin/istioctl version
-./istio-*/bin/istioctl kube-inject -f istio_install/samples/bookinfo/platform/kube/bookinfo.yaml | kubectl apply -n bookstore -f -
+./istio-*/bin/istioctl kube-inject -f istio_install/samples/bookinfo/platform/kube/bookinfo.yaml -o istio_install/samples/bookinfo/platform/kube/bookinfowithsidecar.yaml
+kubectl apply -n bookstore -f istio_install/samples/bookinfo/platform/kube/bookinfowithsidecar.yaml
