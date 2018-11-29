3726aa067b2f328b6f7341b55c26447b10459f30
Andreas Wilke <andreas@twistlock.com>
2018-11-29 12:14:39 +0100

    Updated Bash script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 curl -L https://git.io/getLatestIstio | sh -
 ./istio-*/bin/istioctl version
 ./istio-*/bin/istioctl kube-inject -f istio_install/samples/bookinfo/platform/kube/bookinfo.yaml | kubectl apply -n bookstore -f -
